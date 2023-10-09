9390870e30df126b4565961eef2ec2d0d8d65723
tkaur6494 <64887285+tkaur6494@users.noreply.github.com>
2023-10-09 16:03:16 +0200

    Update streamlit_app.py
    Fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,5 @@ streamlit.text("Item 3")
 
 fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-strea,lit.dataframe(fruit_list)
+streamlit.dataframe(fruit_list)
 
