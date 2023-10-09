b03e169be385771aa5ee15dab213f1df5237f087
tkaur6494 <64887285+tkaur6494@users.noreply.github.com>
2023-10-09 16:06:48 +0200

    Update streamlit_app.py
    Add default value for filter

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/d
 fruit_list = fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(fruit_list.index), ["Avocado", "Strawberries"])
 
 streamlit.dataframe(fruit_list)
 
