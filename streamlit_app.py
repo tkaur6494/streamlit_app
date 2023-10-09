adec3641551737ed788316d6e4e5e9c60a35f7ed
tkaur6494 <64887285+tkaur6494@users.noreply.github.com>
2023-10-09 15:59:26 +0200

    Update streamlit_app.py
    Add content

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,9 @@
+import streamlit
+
+streamlit.title("My Parents new Healthy Dinner")
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Item 1")
+streamlit.text("Item 2")
+streamlit.text("Item 3")
 
